0c107a862fe8fca8857173c45e9434ab980fc070
Kgothatso-create <128015599+Kgothatso-create@users.noreply.github.com>
2023-04-27 13:18:23 +0200

    adding docstring to comments views.py

--- comments/views.py
+++ comments/views.py
@@ -7,6 +7,14 @@ from django.contrib.auth.decorators import login_required
 # The login_required decorator to ensure only logged-in users can access this view
 @login_required(login_url ='login')
 def movie_chat_view(request):
+    """
+    Display a form to allow users to submit a movie chat message and display all movie chat messages.
+
+    :param request: The HTTP request object.
+    :type request: ~django.http.HttpRequest
+    :return: The rendered HTML template for the movie chat page.
+    :rtype: ~django.http.HttpResponse
+    """
     form = MovieChatForm()
     movie_chats = MovieChat.objects.all() # Fetch all MovieChat objects from the database
     if request.method == 'POST':
