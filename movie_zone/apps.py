4d009123181b660dffe4168cc33f0d78a1eafe9d
Kgothatso-create <128015599+Kgothatso-create@users.noreply.github.com>
2023-04-27 13:07:34 +0200

    adding docstring to movie_zone apps.py

--- movie_zone/apps.py
+++ movie_zone/apps.py
@@ -2,5 +2,9 @@ from django.apps import AppConfig
 
 
 class MovieZoneConfig(AppConfig):
+    """
+    This class represents the configuration for the 'movie_zone' app.
+    It sets the default auto field to a big integer field and specifies the name of the app.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'movie_zone'
