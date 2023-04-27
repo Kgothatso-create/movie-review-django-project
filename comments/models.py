a422148c6e42cfa86281fb80348bc36826bd42db
Kgothatso-create <128015599+Kgothatso-create@users.noreply.github.com>
2023-04-27 13:16:58 +0200

    adding docstrings for comments to apps.py and models.py

--- comments/models.py
+++ comments/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 class MovieChat(models.Model):
+    """
+    A model class to represent a movie chat object in the database.
+    Returns a string representation of the MovieChat object.
+    """
     username = models.CharField(max_length=100)
     movie_chat = models.TextField(max_length=500)
 
